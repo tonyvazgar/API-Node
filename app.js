b56b11e7032a0228582e656bc3a70185bb163616
Tony Vazgar <tonyvazgar@icloud.com>
2022-03-28 15:00:05 -0600

    Agregando conexion a bdd

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const cors    = require("cors")
 const app     = express()
+const dbConnect = require("./config/mongo")
 
 app.use(cors())
 
@@ -10,3 +11,5 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
         console.log("Corriendo en http://localhost:" + port);
     });
+
+    dbConnect();
