0b33ff6f7028e879dc524bf14101b78c29160a59
Tony Vazgar <tonyvazgar@icloud.com>
2022-04-07 23:58:25 -0500

    Validaciones de parametros de archivos

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router  = express.Router();
+const {validatorCreateItem} = require('../validators/tracks');
 const {getItems, getItem, createItems} = require("../controllers/tracks");
 
 //TODO http://localhost/tacks GET,POST,DELETE,PUT
 
 router.get('/', getItems);
 router.get('/:id', getItem);
-router.post('/', createItems);
+router.post('/', validatorCreateItem, createItems);
 
 module.exports = router;
