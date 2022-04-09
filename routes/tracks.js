3828de7f90bf6e27c9d145d03752da0d767e4967
Tony Vazgar <tonyvazgar@icloud.com>
2022-04-09 00:09:05 -0500

    Middleware

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router  = express.Router();
+const customHeader = require("../middlewares/customHeader");
 const {validatorCreateItem} = require('../validators/tracks');
 const {getItems, getItem, createItems} = require("../controllers/tracks");
 
@@ -7,6 +8,6 @@ const {getItems, getItem, createItems} = require("../controllers/tracks");
 
 router.get('/', getItems);
 router.get('/:id', getItem);
-router.post('/', validatorCreateItem, createItems);
+router.post('/', validatorCreateItem, customHeader, createItems);
 
 module.exports = router;
