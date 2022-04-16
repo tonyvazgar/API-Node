abbe7bdbf2d4a520840d7d27cb1876b50fb9c66d
Tony Vazgar <tonyvazgar@icloud.com>
2022-04-16 11:15:52 -0500

    Soft Delete y Delete | CRUD en Storage

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,5 +1,9 @@
+const fs = require("fs");
+const { matchedData } = require('express-validator');
 const {storageModel} = require('../models');
+const {handleHttpError} = require("../utils/handleError");
 const PUBLIC_URL = process.env.PUBLIC_URL;
+const MEDIA_URL  = `${__dirname}/../storage`;
 
 /**
  * Obtener lista de la base de datos
@@ -7,9 +11,12 @@ const PUBLIC_URL = process.env.PUBLIC_URL;
  * @param {*} res 
  */
 const getItems = async (req, res) => {
-    // const data = ["holaa", "amigo"];
-    const data = await storageModel.find({});
-    res.send({data});
+    try{
+        const data = await storageModel.find({});
+        res.send({data});
+    }catch(error){
+        handleHttpError(res, "ERROR_GET_ITEMS");
+    }
 };
 
 /**
@@ -17,8 +24,14 @@ const getItems = async (req, res) => {
  * @param {*} req 
  * @param {*} res 
  */
-const getItem = (req, res) => {
-    res.send("Soy un item!");
+const getItem = async (req, res) => {
+    try {
+        const {id} = matchedData(req);
+        const data = await storageModel.findById(id);
+        res.send({data}); 
+    } catch (error) {
+        handleHttpError(res, "ERROR_GET_DETAIL_ITEM");
+    }
 };
 
 /**
@@ -39,12 +52,42 @@ const createItem = async (req, res) => {
 };
 
 /**
- * Borrar un registro
+ * Borrar un registro logicamente
  * @param {*} req 
  * @param {*} res 
  */
-const deleteItems = (req, res) => {
+const softDeleteItem = async (req, res) => {
+    try {
+        const { id } = matchedData(req);
+        const data = await storageModel.delete({_id: id});
+        res.send({data})
+    } catch (error) {
+        handleHttpError(res, "ERROR_SOFT_DELETE_ITEM_STORAGE");
+    }
+};
 
+/**
+ * Borrar un registro de archivo permanenetemente
+ */
+const deleteItem = async (req, res) => {
+    try {
+        const { id } = matchedData(req);
+        const dataFile = await storageModel.findById(id);
+        await storageModel.deleteOne(id);
+
+        const {filename} = dataFile;
+        const filepath   = `${MEDIA_URL}/${filename}`;
+        fs.unlinkSync(filepath);
+
+        const data = {
+            filepath,
+            deleted: 1
+        };
+
+        res.send({data})
+    } catch (error) {
+        handleHttpError(res, "ERROR_DELETE_ITEM_STORAGE");
+    }
 };
 
 /**
@@ -52,9 +95,9 @@ const deleteItems = (req, res) => {
  * @param {*} req 
  * @param {*} res 
  */
-const updateItems = (req, res) => {
+const updateItem = async (req, res) => {
 
 };
 
 
-module.exports = {getItems, getItem, createItem, deleteItems, updateItems};
+module.exports = {getItems, getItem, createItem, deleteItem, updateItem};
--- routes/storage.js
+++ routes/storage.js
@@ -1,9 +1,32 @@
 const uploadMiddleware = require("../utils/handleStorage");
 const express = require('express');
 const router = express.Router();
-const {createItem} = require("../controllers/storage");
+const { createItem, getItems, getItem, updateItem, deleteItem } = require("../controllers/storage");
+const { validatorGetItem } = require("../validators/storage");
 
-
+/**
+ * Subir un archivo
+ */
 router.post('/', uploadMiddleware.single("myfile"), createItem);
+
+/**
+ * Obtener la lista de todos los archivos
+ */
+router.get("/", getItems);
+
+/**
+ * Obtener un archivo
+ */
+router.get("/:id", validatorGetItem, getItem);
+
+/**
+ * Actualizar un archivo
+ */
+router.put("/:id", validatorGetItem, updateItem);
+
+/**
+ * Borrar un archivo
+ */
+router.delete("/:id", validatorGetItem, deleteItem);
 
 module.exports = router;
